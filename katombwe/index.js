f0b4584db7ebb977ffa7bb368e8bf45f6d36eb42
stanytz378 <stanytz378@gmail.com>
2026-05-16 03:41:40 +0300

    Create index.js

---
+++ katombwe/index.js
@@ -0,0 +1,34 @@
+/*****************************************************************************
+ *                                                                           *
+ *                     Developed By STANY TZ                                 *
+ *                                                                           *
+ *  🌐  GitHub   : https://github.com/Stanytz378/iamlegendv2                 *
+ *  ▶️  YouTube  : https://youtube.com/@STANYTZ                              *
+ *  💬  WhatsApp : https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p     *
+ *                                                                           *
+ *    © 2026 STANY TZ. All rights reserved.                                 *
+ *                                                                           *
+ *****************************************************************************/
+
+// AntiLink exports
+export {
+    setAntilinkSetting,
+    getAntilinkSetting,
+    loadAntilinkSettings,
+    saveAntilinkSettings,
+    removeAntilinkSetting
+} from './antilinkhelper.js';
+
+// AntiBadword exports
+export {
+    loadAntibadwordSettings,
+    saveAntibadwordSettings,
+    getAntiBadword,
+    setAntiBadword,
+    removeAntiBadword,
+    incrementWarningCount,
+    resetWarningCount,
+    getWarningCount,
+    containsBadWord,
+    BAD_WORDS
+} from './antibadword.js';
