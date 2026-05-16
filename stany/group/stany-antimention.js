4348c24d87fb011554d82a262a19fd1ed590d391
stanytz378 <stanytz378@gmail.com>
2026-05-17 01:27:12 +0300

    Create stany-antimention.js

---
+++ stany/group/stany-antimention.js
@@ -0,0 +1,11 @@
+╭─┴─❍「 *USER INFO* 」❍
+├ 👤 *Name* : Raisi Wa Wapiga Punyeto
+├ 👑 *User* : OWNER
+╰─┬────❍
+╭─┴─❍「 *BOT INFO* 」❍
+├ 🤖 *App* : MDINYANE
+├ 📌 *Version* : 1.0.0
+├ 👨‍💻 *Owner* : STANY TZ
+├ 🌍 *Mode* : Public
+├ 🔧 *Prefix* : .
+╰─┬────❍
