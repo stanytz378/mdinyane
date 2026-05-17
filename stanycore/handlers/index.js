928a46fb498780be8809a244c513a643e524dd18
stanytz378 <stanytz378@gmail.com>
2026-05-17 23:02:42 +0300

    Update index.js

--- stanycore/handlers/index.js
+++ stanycore/handlers/index.js
@@ -1,5 +1,12 @@
 /*****************************************************************************
  *                     Developed By STANY TZ                                 *
+ *                                                                           *
+ *  🌐  GitHub   : https://github.com/Stanytz378/iamlegendv2                 *
+ *  ▶️  YouTube  : https://youtube.com/@STANYTZ                              *
+ *  💬  WhatsApp : https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p     *
+ *                                                                           *
+ *    © 2026 STANY TZ. All rights reserved.                                 *
+ *                                                                           *
  *****************************************************************************/
 
 export { handleAutoTyping } from './autotyping.js';
