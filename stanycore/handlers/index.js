9460f1b3244a1dfd1f4e4bb5290848361380fd5c
stanytz378 <stanytz378@gmail.com>
2026-05-17 11:57:16 +0300

    Create index.js

---
+++ stanycore/handlers/index.js
@@ -0,0 +1,9 @@
+/*****************************************************************************
+ *                     Developed By STANY TZ                                 *
+ *****************************************************************************/
+
+export { handleAutoTyping } from './autotyping.js';
+export { handleAutoRecording } from './autorecording.js';
+export { handleAutoRead } from './autoread.js';
+export { handleAlwaysOnline, startAlwaysOnline, stopAlwaysOnline } from './alwaysonline.js';
+export { isChatAllowed } from './botmode.js';
