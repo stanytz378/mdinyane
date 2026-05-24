e9871b93f722eb4285e7264a0a1c49bdaf9d386a
stanytz378 <stanytz378@gmail.com>
2026-05-25 01:26:01 +0300

    Update menu.js

--- stany/general/menu.js
+++ stany/general/menu.js
@@ -299,7 +299,7 @@ export default {
         menu += `├ 🏷️ *Name* : ${msg.pushName || 'No Name'}\n`;
         menu += `├ 👑 *Role* : ${isOwnerUser ? 'OWNER 👑' : 'USER 👤'}\n`;
         menu += `├ 💎 *Status* : ${userStatus.status}\n`;
-        menu += `├ 📱 *Your Number:* +${ownerInfo.ownerNumber}
+        menu += `├ 📱 *Number* : ${sender.split('@')[0]}\n`;
         menu += `╰─┬────❍\n`;
         
         // Bot Info
